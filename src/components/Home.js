143150426944363a52fbf94e4491bd881af670b9
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-01-11 01:29:21 +0300

    import and exporting of components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {username, city} from "../data/user"
+console.log(username)
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home
